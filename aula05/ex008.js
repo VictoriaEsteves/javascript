1e2ec26d5bcd3f8dd7d3b9638fbf1a4a9a0c999b
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-07-31 12:16:09 -0300

    Create ex008.js and ex009.js

---
+++ aula05/ex008.js
@@ -0,0 +1,16 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) { //condição simple
+    console.log('Você passou a velocidade permitida. MULTADO!')
+}
+
+/* 
+    Condição simples:
+
+    if (Condição) {
+        | bloco |
+    }
+
+*/
+console.log('Dirija com cuidado')
---
+++ aula05/ex009.js
@@ -0,0 +1,16 @@
+var pais = 'italia'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'Brasil') {
+    console.log('Você é brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}
+
+/*
+condição complexa:
+if (condição) {
+    | |
+} else {
+    | |
+}
+*/
