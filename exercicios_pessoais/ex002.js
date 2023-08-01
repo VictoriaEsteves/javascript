37ce07ba1c6924345b17afb68d2b385ae082cbee
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-01 18:10:23 -0300

    Create ex002.js and ex003.js

---
+++ exercicios_pessoais/ex002.js
@@ -0,0 +1,7 @@
+var numero = 12
+
+if (numero % 2 == 0) {
+    console.log('O numero é par')
+} else {
+    console.log('O Numero é impar')
+}
---
+++ exercicios_pessoais/ex003.js
@@ -0,0 +1,9 @@
+var ptjogo = 99
+
+if (ptjogo >= 90) {
+    console.log("Parabéns, você é um vencedor!")
+} else if (ptjogo >= 70) {
+    console.log("Você mandou bem!")
+} else {
+    console.log("Tente novamente!")
+}
