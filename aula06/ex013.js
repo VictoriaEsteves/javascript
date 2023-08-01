b63c2aec7deb4c7d90e409b02a77932f170e5564
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-01 16:04:56 -0300

    Create ex013.js

---
+++ aula06/ex013.js
@@ -0,0 +1,30 @@
+var hora = 7;
+
+console.log(`Agora são exatamente ${hora} horas`)
+if (hora >= 0 && hora <= 6) {
+    console.log('Boa Madrugada!')
+} else if (hora < 12) {
+    console.log('Boa Manha!')
+} else if (hora <= 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
+
+
+
+// Meu senhor como foi difícil para mim, mas aprendi 
+// Esse comentario de baixo foi quando eu consegui a primeira vez e fui tentar de novo!
+/*
+
+if (hora >= 0 && hora <= 6) {
+    console.log('Boa madrugada!')
+} else if (hora < 12) {
+    console.log('Bom dia!')
+} else if (hora <= 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
+
+*/
