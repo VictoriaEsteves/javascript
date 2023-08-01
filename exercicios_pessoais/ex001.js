8838d4154db10b6275a5af9d83ba6a46eabd617f
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-01 18:00:18 -0300

    Create ex001.js

---
+++ exercicios_pessoais/ex001.js
@@ -0,0 +1,10 @@
+var numero = 0
+
+console.log(`O numero é ${numero} então ele é:`)
+if (numero > 0) {
+    console.log('O Numero é positivo')
+} else if (numero < 0) {
+    console.log('O Numero é Negativo')
+} else {
+    console.log('O numero é o 0')
+}
