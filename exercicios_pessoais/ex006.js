fd8136106c708adebc21cb7b9868b8e49b77ca73
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-02 11:45:39 -0300

    Create ex006.js

---
+++ exercicios_pessoais/ex006.js
@@ -0,0 +1,9 @@
+var idade = 35
+
+if (idade >= 18 && idade <= 25) {
+    console.log(`Sua idade é de ${idade}, você é estudante`)
+} else if (idade >= 60) {
+    console.log(`Sua idade é de ${idade}, você é Idoso`)
+} else {
+    console.log(`Sua idade é de ${idade}, você é não se qualifica para nenhum desconto`)
+}
