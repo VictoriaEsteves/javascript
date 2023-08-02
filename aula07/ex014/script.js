cfd26874fc743b38ccbb7f4f89fcc3fbf25c379a
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-02 14:19:20 -0300

    ex014

--- aula07/ex014/script.js
+++ aula07/ex014/script.js
@@ -1,1 +1,17 @@
-window.alert('Olá')
+function carregar() {
+    
+    var msg = window.document.getElementById('msg')
+    var foto = window.document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `<h2>Agora são ${hora} Horas</h2>`
+
+    if (hora >=0 && hora < 12) {
+        //Bom dia!
+        img.src = 'Foto-manha.jpg'
+    } else if (hora >= 12 && hora < 18) {
+        //Boa tarde
+    } else {
+        //Boa noite
+    }
+}
