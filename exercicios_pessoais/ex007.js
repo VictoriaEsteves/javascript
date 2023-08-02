d54ee5aa44b6f074fe5ac76ab986174a74917827
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-02 11:53:20 -0300

    Create ex007.js

---
+++ exercicios_pessoais/ex007.js
@@ -0,0 +1,8 @@
+var senha = "Paodequeijo222"
+var quantidadeCaracteres = senha.length
+
+if (quantidadeCaracteres < 8) {
+    console.log('Sua senha é fraca, recomendamos uma com 8 letras')
+} else {
+    console.log('Sua senha é forte, parabens')
+}
