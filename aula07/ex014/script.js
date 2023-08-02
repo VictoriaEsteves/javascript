0aa29e85f9ed89588bb34b99ae0446b69c249c73
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-02 14:42:28 -0300

    Fixed bugs

--- aula07/ex014/script.js
+++ aula07/ex014/script.js
@@ -1,17 +1,19 @@
 function carregar() {
     
     var msg = window.document.getElementById('msg')
-    var foto = window.document.getElementById('img')
+    var img = window.document.getElementById('img')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `<h2>Agora são ${hora} Horas</h2>`
 
     if (hora >=0 && hora < 12) {
         //Bom dia!
-        img.src = 'Foto-manha.jpg'
+        img.src = 'imagens/Foto-manha.jpg'
     } else if (hora >= 12 && hora < 18) {
         //Boa tarde
+        img.src = 'imagens/Foto-tarde.jpg'
     } else {
         //Boa noite
+        img.src = 'imagens/Foto-noite.jpg'
     }
 }
