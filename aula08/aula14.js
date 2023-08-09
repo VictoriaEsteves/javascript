89d208858a95792c40afe1aebebe280d2c1234db
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-09 16:30:21 -0300

    Create aula14.js

---
+++ aula08/aula14.js
@@ -0,0 +1,18 @@
+/*
+var c = 1 // incio
+while (c <= 10) //teste {
+    console.log('${c}')
+    c++ // incrementi
+}
+
+estrutura do for:
+
+for (inicio; teste; incremento) {
+    
+}
+
+*/
+
+for (var c=1; c <= 10; c++) {
+    console.log(`Passo ${c}`)
+}
