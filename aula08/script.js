dec9aa8f89856feb0a5aa02bd87432ed7edc144b
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-09 18:01:56 -0300

    ex004.html

---
+++ aula08/script.js
@@ -0,0 +1,22 @@
+
+function contar() {
+    var numini = document.getElementById('inicio') 
+    var numfim = document.getElementById('fim') 
+    var numpass = document.getElementById('passo')
+
+    var res = document.querySelector("div p")
+
+if (numini <=0 || numfim <= 0 || numpass <= 0) {
+    window.alert('Faltam numero')
+} else {
+    res.innerHTML = 'Contando:'
+    var i = Number(numini.value)
+    var f = Number(numfim.value)
+    var p = Number(numpass.value)
+}
+
+for(let c = i; c <= f; c += p) {
+    res.innerHTML += ` ${c} ` 
+}
+    
+}
