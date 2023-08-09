095337154df97d1300eb0111a404478b613b9412
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-09 16:03:02 -0300

    Update ambiente.js

--- aula08/ambiente.js
+++ aula08/ambiente.js
@@ -2,6 +2,7 @@ var c = 1
 while (c <= 20) {
     console.log(`Passo ${c}`)
     c++ // C = c + 1
+    // Se nao tiver o c++ ele quebra mesmo se eu trocar a condição para 1, para que ele reconheça o final da condição.
 }
 
 /*
