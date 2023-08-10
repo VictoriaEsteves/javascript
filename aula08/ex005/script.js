ab2d98b8ae118d895fe15bb1a63bfc6b6c94b829
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-10 15:22:10 -0300

    Update index.html

--- aula08/ex005/script.js
+++ aula08/ex005/script.js
@@ -1,8 +1,14 @@
-
 function Tabuada() {
-    var tabu = document.getElementById('num').value
-    var c = tabu * 10
-    var res = document.querySelector('div#res')
-    
-    
+    let num = document.getElementById('num')
+    let tab = document.getElementById('lstTabu')
+    if (num.value.length == 0){
+        window.alert('Por favor, digite um numero!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        while (c <= 10) {
+            let item = document.createElement('Option')
+            item.text = `${} x ${} = ${}` 
+        }
+    }
 }
