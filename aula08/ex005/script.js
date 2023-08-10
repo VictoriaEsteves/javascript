a7eb04b20bab3c52fc0e4b7b9997f4e50bce1610
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-10 12:24:36 -0300

    Update script.js

--- aula08/ex005/script.js
+++ aula08/ex005/script.js
@@ -1,9 +1,8 @@
-var tabu = document.getElementById('num').value
-var c = tabu * 10
-var res = document.querySelector('div#res')
 
 function Tabuada() {
-    if (tabu < 0) {
-        window.alert('Por favor, coloque um numero positivo')
-    }
+    var tabu = document.getElementById('num').value
+    var c = tabu * 10
+    var res = document.querySelector('div#res')
+    
+    
 }
