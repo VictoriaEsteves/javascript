3e31e58e7e432ae63aa30abfde889cfdee06a597
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-15 20:06:05 -0300

    Update script.js

--- desafios/df01/script.js
+++ desafios/df01/script.js
@@ -3,4 +3,10 @@ function botao1() {
     window.alert('Você clicou no primeiro botão')
 }
 
-function botao2()
+function botao2() {
+    window.alert('Você clicou no segundo botão')
+}
+
+function botao3() {
+    window.alert('Você clicou no terceiro botão')
+}
