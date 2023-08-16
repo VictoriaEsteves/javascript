629ecbf259f39e839771b5b10161afe9b833e4b3
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-15 21:15:13 -0300

    Upgrade script.js

---
+++ desafios/df02/script.js
@@ -0,0 +1,6 @@
+
+function iniciar() {
+   let nome = window.prompt('Qual é o seu nome?')
+   let idade = window.prompt('Qual é sua idade?')
+    window.confirm(`Olá, ${nome} que tem ${idade} anos!`)
+}
