9ac535ce1b8f919f4560b4a2b5049be05edadff9
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-17 21:29:20 -0300

    update ex001

--- aula10/ex01/script.js
+++ aula10/ex01/script.js
@@ -1,11 +1,5 @@
-let num = document.getElementById('num')
-let valores = []
 
-function enviar() {
-    let numdig = Number(num.value)
-    if (numdig == " ") {
-        window.alert('Por favor, digite um numero!')
-    } else if (numdig > 100) {
-        window.alert('Por Favor digite um valor dentro do pedido!')
-    }
+
+function verificar() {
+    
 }
