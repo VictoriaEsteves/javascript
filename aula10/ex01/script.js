f3d574fd546d8df5fa167cee59e54ebc5812dd7b
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-17 22:33:43 -0300

    some changes

--- aula10/ex01/script.js
+++ aula10/ex01/script.js
@@ -1,5 +1,31 @@
+let num = document.querySelector('input#num')
+let list = document.querySelector('lstNum')
+let res = document.querySelector('div#res')
+let valores = []
 
-
-function verificar() {
-    
+function isnumber(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
 }
+
+function inlist(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adcionar() {
+    if(isnumber(num.value) && !inlist(num.value, valores)) {
+        valores.push(Number(num.value))
+        item.text = `Valor ${num.value} adicionado`
+        let item = document.createElement('option')
+        list.appendChild(item)
+    } else {
+        window.alert('Coloque um numero valido!')
+    }
+} 
