618f31ab89352a2ceb73b4b786068ee884610cfb
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-21 00:49:57 -0300

    Update index.js

--- exercicios_pessoais/arrays/index.js
+++ exercicios_pessoais/arrays/index.js
@@ -1,5 +1,5 @@
 let color = ['azul', 'amarelo', 'vermelho', 'verde', 'laranja', 'roxo', 'vermelho-arroxeado', 'vermelho-alaranjado', 'amarelo-esverdeado', 'amarelo-alaranjado', 'azul-arroxeado', 'azul-esverdeado']
 
-for(let c=0; c <=10;c++) {
+for(let c=0; c <=11;c++) {
     console.log(color[c])
 }
