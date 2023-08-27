6105e3c56f572546444d3865a18af6f3e1d5f0d9
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-27 03:40:10 -0300

    update script.js

--- aula10/ex01/script.js
+++ aula10/ex01/script.js
@@ -22,9 +22,26 @@ function inList(n, l) {
 }
 
 function adicionar() {
-    if(isnum(num.value) && !inList(num.value, valores)) {
 
+    if(isnum(num.value) && !inList(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado!`
+        list.appendChild(item)
     } else {
         window.alert('Valor invalido ou já obtido!')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if(valores.length == 0) {
+        window.alert('Adicione dados para fazer os calculos')
+    } else {
+        let tot = valores.length
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} Numeros Cadastrados</p>`
     }
 }
