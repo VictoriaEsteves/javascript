12fc0908042dde6be739e8dd2b6c7e06342aab82
Victoria Esteves <138332052+VictoriaEsteves@users.noreply.github.com>
2023-08-27 04:16:14 -0300

    Update script.js

--- aula10/ex01/script.js
+++ aula10/ex01/script.js
@@ -28,6 +28,7 @@ function adicionar() {
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado!`
         list.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor invalido ou já obtido!')
     }
@@ -41,9 +42,25 @@ function finalizar() {
         window.alert('Adicione dados para fazer os calculos')
     } else {
         let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
 
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor)
+            menor = valores[pos]
+        }
+        media = soma / tot
         res.innerHTML = ''
-        res.innerHTML += `Ao todo temos ${tot} Numeros Cadastrados`
+        res.innerHTML += `<p>Ao todo temos ${tot} Numeros Cadastrados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores da o resultado de ${soma}</p>`
+        res.innerHTML += `<p>A media total foi de ${media}</p>`
     }
 
 }
